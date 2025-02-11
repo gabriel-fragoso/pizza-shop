41a1403d662f25eb2b9d27f3a53202b28cd65573
Gabriel <fragosooliveira773@gmail.com>
2025-02-11 07:33:49 -0300

    feat: add dashboard and login page

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { RouterProvider } from "react-router-dom";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Toaster } from "sonner";
+
+import { router } from "./routes";
+import { ThemeProvider } from "./components/theme-provider";
+
+export function App() {
+  return (
+    <HelmetProvider>
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </HelmetProvider>
+  );
+}
---
+++ src/routes.tsx
@@ -0,0 +1,39 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import { Dashboard } from "./pages/app/dashboard";
+import { SignIn } from "./pages/auth/sign-in";
+import { SignUp } from "./pages/auth/sign-up";
+import { AppLayout } from "./pages/_layouts/app";
+import { AuthLayout } from "./pages/_layouts/auth";
+import { Orders } from "./pages/app/orders/orders";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+      {
+        path: "/orders",
+        element: <Orders />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "/sign-in",
+        element: <SignIn />,
+      },
+      {
+        path: "/sign-up",
+        element: <SignUp />,
+      },
+    ],
+  },
+]);
